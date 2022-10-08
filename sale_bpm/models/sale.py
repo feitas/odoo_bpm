e5c5bbe81da23543f00a39e231aca801b7bb2ad4
hao.chen37 <hao.chen37@cevt.se>
2022-10-08 10:26:20 +0200

    relate sale and pm

--- sale_bpm/models/sale.py
+++ sale_bpm/models/sale.py
@@ -7,10 +7,14 @@ from odoo.exceptions import UserError
 class Sale(models.Model):
     _inherit = ['sale.order']
     
+    pm_process_name = fields.Char(string='PM Related Process Name')
+    pm_activity_id = fields.Many2one('syd_bpm.activity',string="Related Activity")
+    # case_ids = fields.One2many('syd_bpm.case','order_id',string="Cases")
     def btn_start_process(self):
         # get process
-        process = self.env['syd_bpm.process'].search([], limit=1)
+        self.ensure_one()
+        process = self.env['syd_bpm.process'].search([('name','=',self.pm_process_name)], limit=1)
         if process:
-            process.process_group_id.start_process(process, None)
+            process.process_group_id.start_process(process, None,related_model=self._name, related_id=self.id)
         else:
             raise UserError("没有找到工作流！")
